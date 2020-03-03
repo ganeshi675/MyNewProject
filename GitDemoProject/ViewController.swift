214b4c8aa0604d0274159e9efad7047e265feb67
Aman <aman.joshi@ftechiz.com>
2020-03-03 16:36:35 +0530

    Initial Commit

---
+++ GitDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemoProject
+//
+//  Created by Atul Tiwari on 03/03/20.
+//  Copyright © 2020 Atul Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
